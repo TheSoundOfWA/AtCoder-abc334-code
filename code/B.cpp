fe18267bbbb5412cb37a1928ac8f881ac3b3944c
TheSoundOfWA <121793003+TheSoundOfWA@users.noreply.github.com>
2023-12-23 22:07:09 +0800

    Update B.cpp

--- code/B.cpp
+++ code/B.cpp
@@ -4,8 +4,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+
 int main() {
-  
+  ll a, m, l, r;
+  scanf("%lld%lld%lld%lld", &a, &m, &l, &r);
+
+  l -= a;
+  r -= a;
+
+  r += (abs(l) / m + 1) * m;
+  l += (abs(l) / m + 1) * m;
+
+  printf("%lld", r / m - (l - 1) / m);
   
   return 0;
 }

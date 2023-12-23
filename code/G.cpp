cb6daea3a7cac0f21874ba698750542cdee8880c
TheSoundOfWA <121793003+TheSoundOfWA@users.noreply.github.com>
2023-12-23 17:52:53 +0800

    Update G.cpp

--- code/G.cpp
+++ code/G.cpp
@@ -1,5 +1,5 @@
 // User Name: JonathanLiu
-// Link: https://atcoder.jp/contests/abc___/tasks/abc____g
+// Link: https://atcoder.jp/contests/abc334/tasks/abc334_g
 
 #include <bits/stdc++.h>
 using namespace std;

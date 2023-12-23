99b687b34490811ff873d706881b0a54de91b4a3
TheSoundOfWA <121793003+TheSoundOfWA@users.noreply.github.com>
2023-12-23 17:52:53 +0800

    Update F.cpp

--- code/F.cpp
+++ code/F.cpp
@@ -1,5 +1,5 @@
 // User Name: JonathanLiu
-// Link: https://atcoder.jp/contests/abc___/tasks/abc____f
+// Link: https://atcoder.jp/contests/abc334/tasks/abc334_f
 
 #include <bits/stdc++.h>
 using namespace std;

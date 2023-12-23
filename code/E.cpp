c1790b951fef08522ff3aea11378b850b2ac19fc
TheSoundOfWA <121793003+TheSoundOfWA@users.noreply.github.com>
2023-12-23 17:52:52 +0800

    Update E.cpp

--- code/E.cpp
+++ code/E.cpp
@@ -1,5 +1,5 @@
 // User Name: JonathanLiu
-// Link: https://atcoder.jp/contests/abc___/tasks/abc____e
+// Link: https://atcoder.jp/contests/abc334/tasks/abc334_e
 
 #include <bits/stdc++.h>
 using namespace std;
